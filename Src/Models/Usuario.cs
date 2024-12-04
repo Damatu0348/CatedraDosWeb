ffdc2e8303153cd902b094c9773b493ace2481d8
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-04 00:42:11 -0300

    modelo añadido

---
+++ Src/Models/Usuario.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiWebDos.Src.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario {get; set;}
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "El RUT debe tener entre 3 y 20 caracteres.")]
+        public string Nombre {get; set;} = string.Empty;
+        [EmailAddress(ErrorMessage = "Correo electrónico inválido.")]
+        public string Correo {get; set;} = string.Empty;
+        [DataType(DataType.Date)]
+        [CustomValidation(typeof(Usuario), "ValidateBirthDate")]
+        public string FechaNacimiento { get; set; } = string.Empty;
+        [RegularExpression(@"Masculino|Femenino|Otro|Prefiero no decirlo", ErrorMessage = "El género no es válido.")]
+        public string Genero { get; set; } = string.Empty;
+
+        public static ValidationResult? ValidateBirthDate(DateTime FechaNacimiento, ValidationContext context)
+        {
+            return FechaNacimiento < DateTime.Now ? ValidationResult.Success : new ValidationResult("La fecha de nacimiento debe ser menor a la fecha actual.");
+        }
+
+
+    }
+}
